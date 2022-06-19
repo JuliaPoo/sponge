422a3799aa4c8c931a0c0240d5cdcd207ab78790
Samuel Gruetter <saemi472@gmail.com>
2022-06-19 02:20:08 -0400

    wip abstracting over backend

--- plugin/src/test_tactic.ml
+++ plugin/src/test_tactic.ml
@@ -1,6 +1,36 @@
 open Sexplib (* opam install sexplib *)
 open Proofview
 
+
+type assertion =
+  | AEq of Sexp.t * Sexp.t
+  | AProp of Sexp.t
+
+let assertion_to_equality a =
+  match a with
+  | AEq (lhs, rhs) -> (lhs, rhs)
+  | AProp e -> (Sexp.Atom "True", e)
+
+type rule =
+  { rulename: string;
+    quantifiers: string list;
+    sideconditions: assertion list;
+    conclusion: assertion;
+    triggers: Sexp.t list }
+
+type query_accumulator =
+  { declarations: (string, int) Hashtbl.t; (* arity of each function symbol *)
+    rules: rule Queue.t;
+    initial_exprs: Sexp.t Queue.t }
+
+let empty_query_accumulator () =
+  { declarations = Hashtbl.create 20;
+    rules = Queue.create ();
+    initial_exprs = Queue.create () }
+
+let make_rust_valid s =
+  Str.global_replace (Str.regexp "@") "AT" (Str.global_replace (Str.regexp "\\.") "DOT" s)
+
 let proof_file_to_reversed_list filepath f =
   let res = ref [] in
   let chan = open_in filepath in
@@ -20,6 +50,220 @@ let proof_file_to_reversed_list filepath f =
    with End_of_file -> ());
   close_in chan;
   !res
+
+let proof_file_path = "/tmp/egg_proof.txt"
+
+module type BACKEND =
+  sig
+    type t
+    val create: unit -> t
+    val declare_fun: t -> string -> int -> unit
+    val declare_rule: t -> rule -> unit
+    val declare_initial_expr: t -> Sexp.t -> unit
+    val minimize: t -> Sexp.t -> string list
+    val prove_equal: t -> Sexp.t -> Sexp.t -> string list option
+    val reset: t -> unit
+    val close: t -> unit
+  end
+
+(* Once created, supports interactions satisfying the following regex:
+( declare_fun+; declare_rule+; declare_initial_expr+; ( minimize | prove_equal ); reset )
+and the close at the end is optional a no-op. *)
+module RecompilationBackend : BACKEND = struct
+  let needs_multipattern r =
+    (match r.sideconditions with
+     | _ :: _ -> true
+     | [] -> match r.triggers with
+             | _ :: _ -> true
+             | [] -> false)
+
+  let rule_to_macro buf r =
+    if needs_multipattern r then (
+      Buffer.add_string buf {|    coq_rewrite!("|};
+      Buffer.add_string buf r.rulename;
+      Buffer.add_string buf {|", "|};
+      List.iteri (fun i sc ->
+          Buffer.add_string buf "$hyp";
+          Buffer.add_string buf (string_of_int i);
+          Buffer.add_string buf " = ";
+          (match sc with
+           | AEq (lhs, rhs) ->
+              Sexp.to_buffer ~buf lhs;
+              Buffer.add_string buf " = ";
+              Sexp.to_buffer ~buf rhs;
+           | AProp e ->
+              Sexp.to_buffer ~buf e);
+          Buffer.add_string buf ", ";
+        ) r.sideconditions;
+      List.iteri (fun i tr ->
+          Buffer.add_string buf "$trigger";
+          Buffer.add_string buf (string_of_int i);
+          Buffer.add_string buf " = ";
+          Sexp.to_buffer ~buf tr;
+          Buffer.add_string buf ", "
+        ) r.triggers;
+      let (lhs, rhs) = assertion_to_equality r.conclusion in
+      Buffer.add_string buf "$lhs = ";
+      Sexp.to_buffer ~buf lhs;
+      Buffer.add_string buf {|" => "|};
+      Sexp.to_buffer ~buf rhs;
+      Buffer.add_string buf "\");\n"
+    ) else (
+      Buffer.add_string buf {|    rewrite!("|};
+      Buffer.add_string buf r.rulename;
+      Buffer.add_string buf {|", "|};
+      let (lhs, rhs) = assertion_to_equality r.conclusion in
+      Sexp.to_buffer ~buf lhs;
+      Buffer.add_string buf {|" => "|};
+      Sexp.to_buffer ~buf rhs;
+      Buffer.add_string buf "\");\n"
+    )
+
+  type state =
+    | SDeclaringFuns
+    | SDeclaringRules
+    | SDeclaringInitialExprs
+    | SDone
+
+  type t = {
+      buf: Buffer.t;
+      lemma_arity_buf: Buffer.t;
+      is_eq_buf: Buffer.t;
+      mutable state: state }
+
+  let create () =
+    let buf = Buffer.create 10000 in
+begin
+Buffer.add_string buf {|
+#![allow(missing_docs,non_camel_case_types)]
+use crate ::*;
+pub fn get_proof_file_path() -> &'static str {
+  "|};
+Buffer.add_string buf proof_file_path;
+Buffer.add_string buf {|"
+}
+define_language! {
+  pub enum CoqSimpleLanguage {
+    Num(i32),
+|}
+end;
+    let lemma_arity_buf = Buffer.create 1000 in
+    let is_eq_buf = Buffer.create 1000 in
+    { buf; lemma_arity_buf; is_eq_buf; state = SDeclaringFuns }
+
+  let declare_fun t fname arity =
+    match t.state with
+    | SDeclaringFuns ->
+       Buffer.add_string t.buf
+         (Printf.sprintf {|    "%s" = %s([Id; %d]),\n|} fname (make_rust_valid fname) arity)
+    | _ -> failwith "invalid state machine transition"
+
+  let declare_rule t rule =
+    (match t.state with
+     | SDeclaringFuns ->
+        t.state <- SDeclaringRules;
+begin
+Buffer.add_string t.buf
+{|    Symbol(Symbol),
+  }
+}
+
+pub fn make_rules() -> Vec<Rewrite<CoqSimpleLanguage, ()>> {
+  let v  : Vec<Rewrite<CoqSimpleLanguage, ()>> = vec![
+|}
+end
+     | SDeclaringRules -> ()
+     | _ -> failwith "invalid state machine transition");
+    rule_to_macro t.buf rule;
+    Buffer.add_string t.is_eq_buf {|    ("|};
+    Buffer.add_string t.is_eq_buf rule.rulename;
+    Buffer.add_string t.is_eq_buf
+      (match rule.conclusion with
+       | AEq (_, _) -> "\", true),\n"
+       | AProp _ -> "\", false),\n");
+    Buffer.add_string t.lemma_arity_buf {|    ("|};
+    Buffer.add_string t.lemma_arity_buf rule.rulename;
+    Buffer.add_string t.lemma_arity_buf ", ";
+    Buffer.add_string t.lemma_arity_buf (string_of_int (List.length rule.quantifiers));
+    Buffer.add_string t.lemma_arity_buf "),\n"
+
+  let declare_initial_expr t e =
+    (match t.state with
+     | SDeclaringRules ->
+        t.state <- SDeclaringInitialExprs;
+begin
+Buffer.add_string t.buf {|  ];
+  v
+}
+
+pub fn get_lemma_arity(name: &str) -> Option<usize> {
+  let v = vec![
+|};
+Buffer.add_buffer t.buf t.lemma_arity_buf;
+Buffer.add_string t.buf {|  ];
+  let o = v.iter().find(|t| t.0 == name);
+  match o {
+    Some((_, n)) => { return Some(*n); }
+    None => { return None; }
+  }
+}
+
+pub fn is_eq(name: &str) -> Option<bool> {
+  let v = vec![
+|};
+Buffer.add_buffer t.buf t.is_eq_buf;
+Buffer.add_string t.buf {|
+#[allow(unused_variables)]
+pub fn run_simplifier(f_simplify : fn(&str, Vec<&str>) -> (), f_prove : fn(&str, &str, Vec<&str>) -> ()) {
+  let es = vec![
+|}
+end
+     | SDeclaringInitialExprs -> ()
+     | _ -> failwith "invalid state machine transition");
+    Buffer.add_string t.buf {|    "|};
+    Sexp.to_buffer ~buf:t.buf e;
+    Buffer.add_string t.buf "\",\n"
+
+  let minimize t e =
+    (match t.state with
+     | SDeclaringInitialExprs -> ()
+     | _ -> failwith "invalid state machine transition");
+    Buffer.add_string t.buf "  ];\n";
+    Buffer.add_string t.buf "  let st : &str = \"";
+    Sexp.to_buffer ~buf:t.buf e;
+    Buffer.add_string t.buf {|";
+  f_simplify(st, es);
+}
+|};
+    let egg_repo_path = "/home/sam/git/clones/egg" (* adapt as needed *) in
+    let rust_rules_path = egg_repo_path ^ "/src/rw_rules.rs" in
+    let oc = open_out rust_rules_path in
+    Buffer.output_buffer oc t.buf;
+    close_out oc;
+    Printf.printf "Wrote Rust code to %s\n" rust_rules_path;
+    flush stdout;
+    let cargo_command = "cd \"" ^ egg_repo_path ^ "\" && cargo run coq" in
+    let status = Sys.command cargo_command in
+    Printf.printf "Command '%s' returned exit status %d\n" cargo_command status;
+    if status != 0 then failwith "invoking rust failed"
+    else t.state <- SDone;
+    let reversed_pf = proof_file_to_reversed_list proof_file_path (fun l -> l) in
+    List.rev reversed_pf
+
+  let prove_equal t lhs rhs = failwith "not supported yet"
+
+  let reset t =
+    t.state <- SDeclaringFuns;
+    Buffer.clear t.buf;
+    Buffer.clear t.lemma_arity_buf;
+    Buffer.clear t.is_eq_buf
+
+  let close t = ()
+
+end
+
+(* can be switched to another backend *)
+module Backend = RecompilationBackend
 
 let parse_constr_expr s =
   Pcoq.parse_string Pcoq.Constr.constr s
@@ -96,9 +340,6 @@ exception Unsupported
 
 let lookup_name nameEnv index =
   List.nth nameEnv (index - 1)
-
-let make_rust_valid s =
-  Str.global_replace (Str.regexp "@") "AT" (Str.global_replace (Str.regexp "\\.") "DOT" s)
 
 let print_lang arities chan =
   Printf.fprintf chan "define_language! {\n";
@@ -301,7 +542,7 @@ let eggify_hyp env sigma arities qnames is_eq exprs hyp =
      stringify_equality_without_sideconds (name ^ "$def") [] name rhs
     end
 
-let egg_simpl_goal proof_file_path =
+let egg_simpl_goal () =
   Goal.enter begin fun gl ->
     let sigma = Tacmach.project gl in
     let env = Goal.env gl in
--- plugin/src/test_tactic.mli
+++ plugin/src/test_tactic.mli
@@ -1,4 +1,4 @@
-val egg_simpl_goal : string -> unit Proofview.tactic
+val egg_simpl_goal : unit -> unit Proofview.tactic
 
 (* uconstr = Ltac_pretype.closed_glob_constr with holes = CHole,
    open_constr = EConstr.t with holes being evars *)
